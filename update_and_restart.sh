74958284a1da5d039a3618b1107bba1ee129aa8a
Michael Scheiwiller <mi.lappert@gmail.com>
2025-07-02 17:11:50 +0200

    feat: add update_database.sh to be executed in update_and_restart.sh within webhook call

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -9,5 +9,8 @@ source .venv/bin/activate
 # install the dependencies
 pip install -r requirements.txt
 
+# execute the update_database.sh script
+./update_database.sh
+
 # restart the application
 sudo systemctl restart streamlit
